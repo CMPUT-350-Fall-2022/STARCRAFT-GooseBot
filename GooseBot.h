b69347b8ac21ae0620a927ab42eddeb0e7bf2f20
Sean Meyers <smmeyers@ualberta.ca>
2022-11-02 21:21:44 -0600

    Merge Master into this

--- GooseBot.h
+++ GooseBot.h
@@ -1,8 +1,16 @@
+#ifndef GOOSE_BOT_H_
+#define GOOSE_BOT_H_
+
 #include "sc2api/sc2_api.h"
 #include "sc2api/sc2_args.h"
 #include "sc2lib/sc2_lib.h"
 #include "sc2utils/sc2_manage_process.h"
 #include "sc2utils/sc2_arg_parser.h"
+#include "sc2api/sc2_unit.h"
+#include "cpp-sc2/include/sc2api/sc2_interfaces.h"
+#include "sc2api/sc2_unit_filters.h"
+
+using namespace sc2;
 
 class GooseBot : public sc2::Agent {
 
@@ -12,7 +20,16 @@ class GooseBot : public sc2::Agent {
 
         virtual void OnGameStart();
         virtual void OnStep();
-        virtual void OnUnitIdle(const sc2::Unit* unit) final;    
+        virtual void OnUnitIdle(const sc2::Unit* unit) final;
+        virtual void OnGameEnd();
+
+        bool TryMorphStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type = UNIT_TYPEID::ZERG_LARVA);
+	    bool TryMorphOverlord();
+	    size_t countUnitType(UNIT_TYPEID unit_type);
+	    void scout(const Unit* unit);
+
+	    const Unit* FindNearestMineralPatch(const Point2D& start);
     private:
 
 };
+#endif
