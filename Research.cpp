e3451165ecf9273c4821031419e19e628f998c93
Breanne77 <sjodin@ualberta.ca>
2022-12-07 00:02:32 -0700

    Revert "66 make findbasebuildinggrounds return better points"

--- Research.cpp
+++ Research.cpp
@@ -1,61 +1,28 @@
 #include "GooseBot.h"
-//DEV BRANCH
 
 bool GooseBot::ResearchPhase(){
-    if (CanAfford(UPGRADE_ID::OVERLORDSPEED)){
-        bool success = TryResearch(UNIT_TYPEID::ZERG_HATCHERY, ABILITY_ID::RESEARCH_PNEUMATIZEDCARAPACE, UPGRADE_ID::OVERLORDSPEED);
-        if (success){
-            upgraded.push_back(UPGRADE_ID::OVERLORDSPEED);
-        }
-        if (IsBuilt(UNIT_TYPEID::ZERG_SPAWNINGPOOL)){
-            bool success = TryResearch(UNIT_TYPEID::ZERG_SPAWNINGPOOL, ABILITY_ID::RESEARCH_ZERGLINGMETABOLICBOOST, UPGRADE_ID::ZERGLINGMOVEMENTSPEED);
-            if (success){
-                std::cout << "Zergling Speed Increased" << std::endl;
-                upgraded.push_back(UPGRADE_ID::ZERGLINGMOVEMENTSPEED);
-            }
-            return success;
-        }
+    if (research_phase == 1){
+        return TryResearch(UNIT_TYPEID::ZERG_HATCHERY, ABILITY_ID::RESEARCH_PNEUMATIZEDCARAPACE, UPGRADE_ID::OVERLORDSPEED);
+    }else{++research_phase;}
+    if (research_phase == 2){
+        TryResearch(UNIT_TYPEID::ZERG_SPAWNINGPOOL, ABILITY_ID::RESEARCH_ZERGLINGMETABOLICBOOST, UPGRADE_ID::ZERGLINGMOVEMENTSPEED);
+        std::cout << "Zergling Speed Increased" << std::endl;
+        return true;
     }
-    if (IsBuilt(UNIT_TYPEID::ZERG_SPIRE)){
-        UPGRADE_ID flyer_upgrade = UPGRADE_ID::ZERGFLYERWEAPONSLEVEL1;
-        if (IsUpgraded(flyer_upgrade)){
-            flyer_upgrade = UPGRADE_ID::ZERGFLYERWEAPONSLEVEL2;
-        }
-        if (CanAfford(flyer_upgrade)){
-            bool success = TryResearch(UNIT_TYPEID::ZERG_SPIRE, ABILITY_ID::RESEARCH_ZERGFLYERATTACK, flyer_upgrade);
-            if (success){
-                std::cout << "Flyer Attack Increased" << std::endl;
-                upgraded.push_back(flyer_upgrade);
-            }
-            return success;
-        }else {return false;}
-    }
-    if (CanAfford(UPGRADE_ID::CHITINOUSPLATING)){
-        if (IsBuilt(UNIT_TYPEID::ZERG_ULTRALISKCAVERN)){
-            bool success = TryResearch(UNIT_TYPEID::ZERG_ULTRALISKCAVERN, ABILITY_ID::RESEARCH_CHITINOUSPLATING, UPGRADE_ID::CHITINOUSPLATING);
-            if (success){
-                std::cout << "Ultralisk Armor Increased" << std::endl;
-                upgraded.push_back(UPGRADE_ID::CHITINOUSPLATING);
-            }
-            return success;
-        }
-    }else{return false;}
+    return false;
 }
 
 bool GooseBot::TryResearch(UNIT_TYPEID researcher_type, ABILITY_ID ability, UPGRADE_ID upgrade){
-    if (IsUpgraded(upgrade) || actionPending(ability)) {
+    if (actionPending(ability)
+        || (std::find(upgraded.begin(), upgraded.end(), upgrade) == upgraded.end())) {
         return false;
     }
     const Unit* researcher = FindUnit(researcher_type);
-    if (researcher != nullptr) {
+    if (researcher != nullptr && CanAfford(upgrade)) {
         Actions()->UnitCommand(researcher, ability);
         return true;
     }
     else {
         return false;
     }
-}
-
-bool GooseBot::IsUpgraded(UPGRADE_ID to_test){
-    return (std::find(upgraded.begin(), upgraded.end(), to_test) != upgraded.end());
 }
--- UnitHandling.cpp
+++ UnitHandling.cpp
@@ -1,5 +1,4 @@
 #include "GooseBot.h"
-//DEV BRANCH
 
 // Distribute workers over vespene geysers for harvesting
 bool GooseBot::TryHarvestVespene() {
@@ -27,6 +26,59 @@ bool GooseBot::TryHarvestVespene() {
     Actions()->UnitCommand(unit, ABILITY_ID::SMART, vespene_target);
     return true;
       
+}
+
+// Distribute workers over vespene geysers for harvesting
+bool GooseBot::TryDistributeMineralWorkers() {
+    Units bases = Observation()->GetUnits(Unit::Alliance::Self, IsUnits(baseTypes));
+    if (bases.empty() || num_bases == 1) {
+        return false;
+    }
+    std::cout << "Trying to distribute workers ... ";
+    Units available_workers;
+    int surplus = 0;
+    int total_needed = 0;
+    for (auto base : bases){
+        if (base->build_progress != 1){
+            std::cout << "base not finished" << std::endl;
+            return false;
+        }
+        int workers_needed = base->ideal_harvesters - base->assigned_harvesters;
+        total_needed += workers_needed;
+        if (workers_needed < 0){
+            surplus += (-workers_needed);
+            for (int i = 0; i < surplus; ++i){
+                const Unit* available = FindNearestAllied(drone, base->pos);
+                if (available != nullptr){
+                    available_workers.push_back(available);
+                }
+            }
+        }
+    }
+    if (total_needed <= 0){
+        std::cout << "None needed" << std::endl;
+        return false;
+    }
+    for (auto base : bases){
+        int workers_needed = base->ideal_harvesters - base->assigned_harvesters;
+        std::cout << "workers needed: " << workers_needed << "... ";
+        if (workers_needed > 0 && (!available_workers.empty())){
+            auto a = available_workers.begin();
+            const Unit* mineral_target = FindNearestMineralPatch(base->pos);
+            if (Units(a, available_workers.end()).size() >= workers_needed){
+                Actions()->UnitCommand(Units(a, a + workers_needed), ABILITY_ID::SMART, mineral_target);
+                a += workers_needed;
+                std::cout << "Workers distributing [mid vector]" << std::endl;
+                return true;
+            }else{
+                Actions()->UnitCommand(Units(a, available_workers.end()), ABILITY_ID::SMART, mineral_target);
+                std::cout << "Workers distributing [end vector]" << std::endl;
+                return true;
+            }
+        }
+    }
+    std::cout << "failed" << std::endl;
+    return false;   
 }
 
 // Try to birth a queen unit from a base
